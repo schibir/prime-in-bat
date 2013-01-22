5a23aabe9c86f82ab8bd4063f0b96656ba118a0b
Sergey Chibiryaev <zav.gamedev@gmail.com>
2013-01-22 15:52:22 +0300

    work

--- main.bat
+++ main.bat
@@ -1,5 +1,20 @@
 @echo off
 
-echo ewtwt
+for /L %%j in (2, 1, 10) do (
+	set flag = false
+	call :is_prime %%j
+)
 
 pause
+
+:is_prime
+	for /L %%i in (2, 1, %1) do (
+		call :foo %1 %%i
+	)
+goto :eof
+
+:foo
+	set /a a = %1 / %2
+	echo %a% %1 %2
+goto :eof
+
